09b65f1d23ec42df2bf594123f2f82037d645adb
PauMionez <pmnzzz28@gmail.com>
2024-10-25 17:33:31 +0800

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,28 +20,7 @@ namespace Training_Quest3
         }
 
 
-        #region Input regex
-
-        private void AlphanumericOnly(object sender, TextCompositionEventArgs e)
-        {
-            // Check if the input is alphanumeric (letters or digits)
-            if (!System.Text.RegularExpressions.Regex.IsMatch(e.Text, $"^[a-za-z0-9]+$"))
-                e.Handled = true; // Reject the input
-        }
-        private void NumericOnly(object sender, TextCompositionEventArgs e)
-        {
-            // Check if the input is numeric
-            if (!System.Text.RegularExpressions.Regex.IsMatch(e.Text, "^[0-9\\s?]+$"))
-                e.Handled = true; // Reject the input
-        }
-        private void AlphabeticOnly(object sender, TextCompositionEventArgs e)
-        {
-            // Check if the input is alphabetic (letters only)
-            if (!System.Text.RegularExpressions.Regex.IsMatch(e.Text, $@"[^\d]+"))
-                e.Handled = true; // Reject the input
-        }
-
-        #endregion
+        
 
     }
 }
